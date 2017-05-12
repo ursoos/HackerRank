f4e530abbe75bdb0d713cedade3203c27155b82c
Ursus <ursoos@gmail.com>
2017-05-12 23:49:50 +0200

    test

--- src/main/java/AppleAndOrange.java
+++ src/main/java/AppleAndOrange.java
@@ -1,7 +1,8 @@
-package PACKAGE_NAME;
-
 /**
- * Created by piotr_000 on 5/12/2017.
+ * Created by Ursus on 5/12/2017.
  */
 public class AppleAndOrange {
+    public static void main (String [] args){
+        
+    }
 }
